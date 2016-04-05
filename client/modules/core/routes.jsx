3ab4698f81f428e1ac937fb4dfa6bb94b5599360
PolGuixe <pol.guixe@gmail.com>
2016-04-05 13:32:02 +0200

    added basic user packages and configuration
    created route as well

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,6 +3,8 @@ import {mount} from 'react-mounter';
 
 import MainLayout from './components/main_layout.jsx';
 import Home from './components/home.jsx';
+import SignIn from './components/sign_in.jsx';
+
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -12,6 +14,14 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       mount(MainLayoutCtx, {
         content: () => (<Home />)
+      });
+    }
+  });
+
+  FlowRouter.route('/sign-in',{
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<SignIn />)
       });
     }
   });
