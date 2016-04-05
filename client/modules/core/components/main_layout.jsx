b6ef2d53a8be4f62ba84970e598d10515324ff03
PolGuixe <pol.guixe@gmail.com>
2016-04-05 11:54:55 +0200

    added basic navbar

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Navbar from './navbar.jsx';
 
-const Layout = ({content = () => null }) => (
+const Layout = ({
+  content = () => null
+}) => (
   <div>
+    <Navbar />
     <div>
       {content()}
     </div>
