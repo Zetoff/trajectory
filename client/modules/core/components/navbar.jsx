6e9d0332a790a4fe2d0f29851a0337a417979c95
PolGuixe <pol.guixe@gmail.com>
2016-04-06 13:30:42 +0200

    removed placeholders from navbar and added non reactive user

--- client/modules/core/components/navbar.jsx
+++ client/modules/core/components/navbar.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
-const NavbarInstance = ({content = () => null}) => (
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">Trajectory</a>
-      </Navbar.Brand>
-      <Navbar.Toggle/>
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider/>
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+class NavbarInstance extends React.Component {
+  render(){
+    const user = Meteor.user();
+    return(
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href={FlowRouter.path('home')}>Trajectory</a>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">{user ? user.profile.name : 'Login'}</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+)
+  }
+}
 
 export default NavbarInstance;
