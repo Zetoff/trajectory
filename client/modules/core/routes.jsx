0781d5a6383915877a87885c4899eb00f54ce65f
PolGuixe <pol.guixe@gmail.com>
2016-04-08 02:00:15 +0200

    very simple test but it's working
    no as good as it should. 
    -user is not reactive

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -5,24 +5,30 @@ import MainLayout from './components/main_layout.jsx';
 import Home from './components/home.jsx';
 import Login from './components/login.jsx';
 
+export default function(injectDeps, {FlowRouter}) {
+    const MainLayoutCtx = injectDeps(MainLayout);
 
-export default function (injectDeps, {FlowRouter}) {
-  const MainLayoutCtx = injectDeps(MainLayout);
+    FlowRouter.route('/', {
+        name: 'home',
+        action() {
+            if (Meteor.user())
+            {
+                mount(MainLayoutCtx, {
+                    content: () => (<Home/>)
+                });
+            } else {
+              mount(MainLayoutCtx, {
+                  content: () => (<Login/>)
+              });
+            }
+        }
+    });
 
-  FlowRouter.route('/', {
-    name: 'home',
-    action() {
-      mount(MainLayoutCtx, {
-        content: () => (<Home />)
-      });
-    }
-  });
-
-  FlowRouter.route('/login',{
-    action() {
-      mount(MainLayoutCtx, {
-        content: () => (<Login />)
-      });
-    }
-  });
+    FlowRouter.route('/login', {
+        action() {
+            mount(MainLayoutCtx, {
+                content: () => (<Login/>)
+            });
+        }
+    });
 }
