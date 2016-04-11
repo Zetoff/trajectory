0f016fc95abff151bfbaa0b82b4a35922f2bc538
PolGuixe <pol.guixe@gmail.com>
2016-04-11 18:03:53 +0200

    Now different pages can have different titles using react-helmet

--- client/modules/core/components/profile.jsx
+++ client/modules/core/components/profile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 class Profile extends React.Component {
   render() {
+    const Head = <Helmet
+      title="Trajectory profile"
+      />
 
     return (
       <div>
+        {Head}
         <h1>Dummy content for the profile page</h1>
       </div>
     );
