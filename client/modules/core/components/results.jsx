098e3669b643b3565bddecdcdf773ac2f2cf2823
PolGuixe <pol@zetoff.com>
2016-04-25 11:25:29 +0200

    basic style improvements

--- client/modules/core/components/results.jsx
+++ client/modules/core/components/results.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
 import rd3 from 'rd3';
 
 const PieChart = rd3.PieChart;
@@ -25,9 +26,13 @@ class Results extends React.Component {
 
     render() {
         return (
-            <div>
-                <PieChart data={this.getData()} width={400} height={400} radius={100} innerRadius={20} sectorBorderColor="white" title="frontEnd FrameWork"/>
-            </div>
+            <Grid>
+              <Row>
+                <Col sm={6} smOffset={3} xs={12}>
+                <PieChart data={this.getData()} width={400} height={400} radius={100} innerRadius={50} sectorBorderColor="white" title="frontEnd FrameWork"/>
+                </Col>
+              </Row>
+            </Grid>
         );
     }
 }
--- client/modules/core/components/survey-test.jsx
+++ client/modules/core/components/survey-test.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import {Input} from 'react-bootstrap';
+import {Input, Grid, Row, Col} from 'react-bootstrap';
 
 class Survey extends React.Component {
   render() {
     const { error } = this.props;
 
     return (
-     <form onSubmit={this.saveSurvey.bind(this)}>
-       <h2>Which front-end framework are you using?</h2>
-       {error ? <p style={{color:'red'}}>{error}</p> : null }
-       <Input type='select' ref='frontEnd'>
-         <option value='blaze'>Blaze</option>
-         <option value='angular'>Angular</option>
-         <option value='react'>React</option>
-       </Input>
-       <Input type='submit'/>
-     </form>
+      <Grid>
+        <Row>
+          <Col sm={6} smOffset={3} xs={12}>
+           <form onSubmit={this.saveSurvey.bind(this)}>
+             <h2>Which front-end framework are you using?</h2>
+             {error ? <p style={{color:'red'}}>{error}</p> : null }
+             <Input type='select' ref='frontEnd'>
+               <option value='blaze'>Blaze</option>
+               <option value='angular'>Angular</option>
+               <option value='react'>React</option>
+             </Input>
+             <Input type='submit' bsStyle='success'/>
+           </form>
+          </Col>
+       </Row>
+     </Grid>
     );
   }
 
