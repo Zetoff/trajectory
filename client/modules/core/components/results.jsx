f2bc38a3743d9d80ed5ec3fe950e4751319ce157
PolGuixe <pol@zetoff.com>
2016-04-25 09:59:07 +0200

    first chart
    -added package
    -basic resutls page

--- client/modules/core/components/results.jsx
+++ client/modules/core/components/results.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import {DoughnutChart} from 'react-chartjs';
+import rd3 from 'rd3';
 
+const PieChart = rd3.PieChart;
 
 class Results extends React.Component {
 
-  getData(){
-    const {results} = this.props;
-    let data = [
-      { label: 'Blaze',
-        value: results.blaze
-      },
-      {
-        label: 'Angular',
-        value: results.angular
-      },
-      {
-        label: 'React',
-        value: results.react
-      }
-    ];
-    console.log(data);
-    return data;
-  }
+    getData() {
+        const {results} = this.props;
+        let data = [
+            {
+                label: 'Blaze',
+                value: results.blaze
+            }, {
+                label: 'Angular',
+                value: results.angular
+            }, {
+                label: 'React',
+                value: results.react
+            }
+        ];
+        console.log(data);
+        return data;
+    }
 
-  render() {
-    return (
-      <div>
-        <DoughnutChart width="600" height="250"/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <PieChart data={this.getData()} width={400} height={400} radius={100} innerRadius={20} sectorBorderColor="white" title="frontEnd FrameWork"/>
+            </div>
+        );
+    }
 }
 
 export default Results;
